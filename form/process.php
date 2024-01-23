7ddfb8a24c04d66521b693237d41df848e5438c9
Anton <antonkerus@gmail.com>
2024-01-23 09:33:14 +0100

    first

---
+++ form/process.php
@@ -0,0 +1,5 @@
+<?php
+
+//This file is for process de form and send to database
+
+echo "This file is for process de form and send to database";
---
+++ second.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    add line for call css style here
+
+    <title>Practice </title>
+</head>
+<body>
+
+<h1>practice with git</h1>
+
+<h2>Instructions</h2>
+
+<p> Your mission.....   in this exercise you need som steps..........  </p>
+<ol>
+    <li>Create classes for your corresponding p element</li>
+    <li>In the css file call second write rules for change the p style</li>
+</ol>
+
+
+<p>10</p>
+<p>9</p>
+<p>8</p>
+<p>7</p>
+<p>6</p>
+<p>5</p>
+<p>4</p>
+<p>3</p>
+<p>2</p>
+
+<p>When finish this, do it a commit and push the changes to the repository.</p>
+
+</body>
+</html>
