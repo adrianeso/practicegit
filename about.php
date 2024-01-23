61c51fbb82f8f173d40f7967065ff0aa9dc6e095
Adrián Esneyder Salcedo Otero <adrianeso@gmail.com>
2024-01-23 12:45:41 +0100

    pausa

---
+++ about.php
@@ -0,0 +1,17 @@
+<?php
+    include 'header.php';
+?>
+
+    <h1>About page</h1>
+
+    <div class="boxes">
+        <div class="box">box1</div>
+        <div class="box">box2</div>
+        <div class="box">box3</div>
+    </div>
+
+
+
+<?php
+    require 'footer.php';
+?>
---
+++ footer.php
@@ -0,0 +1,13 @@
+<div class="footer">
+
+
+    <div><?php echo "this is a php value" ?></div>
+    <div>Widget 2</div>
+    <div>Widget 3</div>
+    <div>Widget 4</div>
+</div>
+
+
+
+</body>
+</html>
