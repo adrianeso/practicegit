658d33a0396a7c31bd0285435240b8a0af148c1d
Adrián Esneyder Salcedo Otero <adrianeso@gmail.com>
2024-01-23 13:57:40 +0100

    pausa

--- about.php
+++ about.php
@@ -1,11 +1,12 @@
 <?php
+error_reporting(E_NOTICE);
     include 'header.php';
 ?>
 
     <h1>About page</h1>
 
     <div class="boxes">
-        <div class="box">box1</div>
+        <div class="box"><?php echo name(); ?> /div>
         <div class="box">box2</div>
         <div class="box">box3</div>
     </div>
@@ -14,4 +15,42 @@
 
 <?php
     require 'footer.php';
+?>
+
+<?php
+
+    $data = [];
+
+    $string = 'hello how are you';
+    $text = explode(" " , $string);
+
+    function name()
+    {
+        return "any value for this element";
+    }
+
+
+    echo count($data);
+    echo '<br>';
+    var_dump($text);
+    echo '<br>';
+
+    var_dump('This is a text');
+
+    echo '<br>';
+    echo '<br>';
+    echo '<br>';
+    echo '<br>';
+    echo '<br>';
+    echo '<br>';
+
+
+
+
+
+
+
+
+
+
 ?>
--- only.php
+++ only.php
@@ -1,1 +1,26 @@
 <?php
+
+$data = [1,2,3,'my text', false];
+
+$asociativeData = [
+    'name' => 'My name',
+    'lastname' => 'My lastname',
+    'address' => [
+        'street_1' => "street one",
+        'street_2' => "street two",
+        'phoneNumber' => '+123456789',
+    ],
+];
+
+/*for($i=0; $i<=count($data); $i++){
+    echo $data[$i] .'<br>';
+}*/
+
+
+foreach ($asociativeData as $key => $value){
+
+}
+
+
+
+
